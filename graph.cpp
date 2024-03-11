a559497267213f14dc8a7ff4946a699f2b81597e
MashaTrunina <147832180+MashaTrunina@users.noreply.github.com>
2024-03-11 21:26:40 +0300

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -8,5 +8,17 @@ const int MAX_VERTICES = 500;
 const double K = 0.1; // коэффициент отталкивания
 const double L= 100.0; // расстояние м/д вершинами
 const double EPS = 1e-5; // сравнение с погрешностью
+void readGraph(std::string grap, std::vector<Vertex>& vertices)
+{
+  std::ifstream infile()
 
+  
+  
+}
+
+
+
+
+
+void saveBMP(const std::vector<Vertex>& vertices, const std::string& filename);
 
