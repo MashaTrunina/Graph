395f713ac46e8522ffdad35c4a0af7a7dffd879a
MashaTrunina <147832180+MashaTrunina@users.noreply.github.com>
2024-03-27 22:01:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
-#include "BMPCreat.h"
+#include "BMPCreate.h"
 #include "TXTReader.h"
+
+template<typename T>
+std::string to_string(T value) {
+    std::ostringstream os;
+    os << value;
+    return os.str();
+}
+int main() {
+    int numVertices, width, height;
+    std::vector<Edge> edges;
+    readInputFromFile("input.txt", numVertices, width, height, edges);
+
+    std::vector<Vertex> vertices;
+
+    srand(time(NULL));
+    for (int i = 0; i < numVertices; ++i) {
+        vertices.push_back({ rand() % width, rand() % height, to_string(i) });
+    }
+
+    BMPGenerator bmpGenerator(width, height, vertices, edges);
+    bmpGenerator.generate("graph.bmp");
+
+    return 0;
+}
+    
